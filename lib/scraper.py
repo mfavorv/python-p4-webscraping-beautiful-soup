4861135a8538726bc8531d8b7ae0ef4b8336ce6f
Favor Vincent <vfavorm77@gmail.com>
2024-06-29 11:19:35 +0300

    webscraping code-along complete

--- lib/scraper.py
+++ lib/scraper.py
@@ -4,3 +4,9 @@ import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+doc = BeautifulSoup(html.text, 'html.parser')
+print(doc.select('.heading-primary'))
+name = doc.select('.heading-primary')[0].name
+print(name)
+attr = doc.select('.heading-primary')[0].attrs
+print(attr)
